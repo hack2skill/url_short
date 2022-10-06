923f386ae6305d83b7994db61b3d218417fee1a1
saxenaprachi <saxenaprachi963@gmail.com>
2022-10-06 11:55:32 +0530

    modified- client- frontend improved

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function App() {
   const [shortUrls, setShortUrls] = useState([]);
@@ -24,29 +44,42 @@ function App() {
   }
   return (
     <>
-    <div>
-      <h1>URL Shrinker</h1>
-      <form method='POST' action='http://localhost:5000/shortUrls'>
-        <label > URL: </label>
-        <input type="url" name='fullUrl' id='fullUrl'/>
-        <button type='submit'> Shrink </button>
+    <div className='w-100 h-screen gap-8 flex flex-col justify-center items-center'>
+      <div className='text-center'>
+        <h1 className='text-2xl text-blue-600/75 font-bold'>Create short URls with Hack2skill!</h1>
+        <p>Hack2skill made making uRls shorter with just one click.</p>
+      </div>
+      <form method='POST' action='http://localhost:5000/shortUrls' className='flex gap-4 bg-white p-3 rounded justify-center items-center  mx-2 formContainer'>
+        <input type="url" name='fullUrl' id='fullUrl' className='inputField p-2' placeholder='Paste Your Link Here...'/>
+        <button type='submit' className='bg-blue-600 text-white p-2 rounded-md text-sm'> Shrink </button>
       </form>
-      <table>
-        <thead>
-          <td>Full Url</td>
-          <td>Short Url </td>
-          <td>Clicks</td>
-        </thead>
-        <tbody>
-          {shortUrls.map((shortUrl) => (
-            <tr>
-              <td>{shortUrl.full}</td>
-              <td onClick={()=>redirectShort(shortUrl.short)}>{shortUrl.short}</td>
-              <td>{shortUrl.clicks}</td>
-            </tr>
+      <TableContainer component={Paper} className='table rounded-md'>
+      <Table  aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell><strong>S.No</strong></TableCell>
+            <TableCell align="right"><strong>Full URL</strong></TableCell>
+            <TableCell align="right"><strong>Short URL</strong></TableCell>
+            <TableCell align="right"><strong>Clicks</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {shortUrls.map((shortUrl, k) => (
+            <TableRow
+              key={k}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {k}
+              </TableCell>
+              <TableCell align="right">{shortUrl.full}</TableCell>
+              <TableCell align="right" onClick={()=>redirectShort(shortUrl.short)} role="button">http://localhost:5000/{shortUrl.short}</TableCell>
+              <TableCell align="right">{shortUrl.clicks}</TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
     </>
   );
