4a1c8c990deab55e9bea0a11a9feae2881eaeb94
Aadarsh Kashyap <aadarshkashyap51@gmail.com>
2022-10-31 14:10:58 +0530

    Design: Added Responsie Header with nav links and login button with a Footer with social media icons.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Header from "./Header";
+import App from "./App";
+import Footer from "./Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
-
